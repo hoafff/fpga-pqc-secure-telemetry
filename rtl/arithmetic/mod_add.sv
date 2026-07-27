fa493b59a1c2d81c253a8bf2fdb76d78d1cf571f
Hoaf <134198196+hoafff@users.noreply.github.com>
2026-07-28 00:32:47 +0700

    fix: make modular add width handling explicit

--- rtl/arithmetic/mod_add.sv
+++ rtl/arithmetic/mod_add.sv
@@ -7,13 +7,17 @@ module mod_add #(
     output logic [WIDTH-1:0] y_o
 );
     logic [WIDTH:0] sum_ext;
+    logic [WIDTH:0] reduced_ext;
+    localparam logic [WIDTH:0] MODULUS_EXT = {1'b0, MODULUS[WIDTH-1:0]};
 
     always_comb begin
         sum_ext = {1'b0, a_i} + {1'b0, b_i};
 
-        if (sum_ext >= MODULUS)
-            y_o = sum_ext - MODULUS;
+        if (sum_ext >= MODULUS_EXT)
+            reduced_ext = sum_ext - MODULUS_EXT;
         else
-            y_o = sum_ext[WIDTH-1:0];
+            reduced_ext = sum_ext;
+
+        y_o = reduced_ext[WIDTH-1:0];
     end
 endmodule
